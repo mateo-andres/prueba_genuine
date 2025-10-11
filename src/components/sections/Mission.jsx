fb67ce6267427fd6edc0e59e279ea14682bc9517
mateo <aniampira@gmail.com>
2025-10-11 09:16:31 -0500

    style(mission, whoWeAre): fix bottom images position based on design

--- src/components/sections/Mission.jsx
+++ src/components/sections/Mission.jsx
@@ -3,40 +3,46 @@ import Vision from "../icons/Vision";
 
 export default function Mission() {
   return (
-    <section className="grid gap-4 p-6 to-blue-200 from-white  via-white bg-linear-to-t rounded-lg">
-      <h2 className="text-2xl font-bold">Nuestra misión en el mundo y lo que soñamos para tus hijos</h2>
-      <iframe
-        className="place-self-center"
-        src="https://www.youtube.com/embed/0IlUVfWThdo?si=_onK2koed97Y4oM9"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
-      ></iframe>
-      <article className="border-3 rounded-lg p-4 border-blue-900">
-        <figure className="py-6 p-2">
-          <Vision />
-        </figure>
-        <h3 className="text-2xl font-bold">Nuestra visión</h3>
-        <p>
-          Queremos educar a miles de niños y jóvenes con los más altos estándares de calidad y pertinencia para que <b>descubran su felicidad</b> y
-          que a partir de sus competencias, sean agentes de cambio en sus regiones por medio de la <b>tecnología, la innovación</b> y el{" "}
-          <b>emprendimiento</b>.
-        </p>
-      </article>
-      <article className="border-3 rounded-lg p-4 border-blue-900">
-        <figure className="py-6 p-2">
-          <Mision />
-        </figure>
-        <h3 className="text-2xl font-bold">Nuestra visión</h3>
-        <p>
-          <b>Educar, empoderar y conectar</b> a nuestros estudiantes mediante experiencias de aprendizaje remoto que trasciendan las fronteras
-          geográficas e idiomáticas, para inspirarlos a transformar el mundo a través de <b>la tecnología, la innovación</b> y el{" "}
-          <b>emprendimiento</b>.
-        </p>
-      </article>
-      <img className="" src="/src/assets/images/jet-pack.png" alt="man in a Jet pack" />
-    </section>
+    <div>
+      <section className="grid gap-4 p-6 to-blue-100 from-white  via-white bg-linear-to-t rounded-t-lg">
+        <h2 className="text-2xl font-bold">Nuestra misión en el mundo y lo que soñamos para tus hijos</h2>
+        <iframe
+          height={215}
+          width="95%"
+          className="place-self-center"
+          src="https://www.youtube.com/embed/0IlUVfWThdo?si=_onK2koed97Y4oM9"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+        <article className="border-3 rounded-lg p-4 border-blue-900">
+          <figure className="py-6 p-2">
+            <Vision />
+          </figure>
+          <h3 className="text-2xl font-bold mb-3">Nuestra visión</h3>
+          <p className="text-base/5">
+            Queremos educar a miles de niños y jóvenes con los más altos estándares de calidad y pertinencia para que <b>descubran su felicidad</b> y
+            que a partir de sus competencias, sean agentes de cambio en sus regiones por medio de la <b>tecnología, la innovación</b> y el{" "}
+            <b>emprendimiento</b>.
+          </p>
+        </article>
+        <article className="border-3 rounded-lg p-4 border-blue-900">
+          <figure className="py-6 p-2">
+            <Mision />
+          </figure>
+          <h3 className="text-2xl font-bold mb-3">Nuestra visión</h3>
+          <p className="text-base/5">
+            <b>Educar, empoderar y conectar</b> a nuestros estudiantes mediante experiencias de aprendizaje remoto que trasciendan las fronteras
+            geográficas e idiomáticas, para inspirarlos a transformar el mundo a través de <b>la tecnología, la innovación</b> y el{" "}
+            <b>emprendimiento</b>.
+          </p>
+        </article>
+      </section>
+      <section className="bg-white rounded-b-lg">
+        <figure className=" w-full h-[134px] bg-[url('/src/assets/images/jet-pack.png')] bg-cover bg-top"></figure>
+      </section>
+    </div>
   );
 }
--- src/components/sections/WhoWeAre.jsx
+++ src/components/sections/WhoWeAre.jsx
@@ -2,20 +2,22 @@ import Play from "../icons/Play";
 
 export default function WhoWeAre() {
   return (
-    <section className="bg-white rounded-lg p-6 grid gap-4">
-      <h2 className="text-2xl font-bold">¿Quiénes somos?</h2>
-      <p>
-        Somos un colegio virtual privado estadounidense constituido y registrado en el estado de <b>La Florida con el código 8822.</b>
-      </p>
-      <h5 className="font-bold text-center mt-4 text-lg">Consulta el registro legal aquí</h5>
-      <button className="bg-blue-900 text-white px-6 py-3 rounded-lg font-bold w-fit mx-auto">Registro legal</button>
-      <div className="flex flex-col p-4 rounded-lg items-center gap-2 bg-purple-300 text-lg text-center font-medium">
+    <div>
+      <section className="bg-white rounded-t-lg p-6 grid gap-4">
+        <h2 className="text-2xl font-bold">¿Quiénes somos?</h2>
         <p>
-          Mira como encontrar el registro legal de <b>Genuine</b>
+          Somos un colegio virtual privado estadounidense constituido y registrado en el estado de <b>La Florida con el código 8822.</b>
         </p>
-        <Play />
-      </div>
-      <img className="rounded-b-lg" src="/src/assets/images/foto_bilinguismo.jpg" alt="billingual teacher with students" />
-    </section>
+        <h5 className="font-bold text-center mt-4 text-lg">Consulta el registro legal aquí</h5>
+        <button className="bg-blue-900 text-white px-6 py-3 rounded-lg font-bold w-fit mx-auto">Registro legal</button>
+        <div className="flex flex-col p-4 rounded-lg items-center gap-2 bg-purple-300 text-lg text-center font-medium">
+          <p>
+            Mira como encontrar el registro legal de <b>Genuine</b>
+          </p>
+          <Play />
+        </div>
+      </section>
+      <img className="rounded-b-lg" src="/src/assets/images/foto_bilinguismo.jpeg" alt="billingual teacher with students" />
+    </div>
   );
 }
