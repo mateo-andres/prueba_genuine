995a5dd1e49a3e3b09bdd51b39f74bb52474f307
mateo <aniampira@gmail.com>
2025-10-11 10:54:32 -0500

    style(header): update header background image and adjust layout styles

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -2,14 +2,14 @@ import LogoGE from "../icons/logo_GE";
 
 export default function Header() {
   return (
-    <header className="grid grid-rows-2 bg-[url('/src/assets/images/happy_family.jpg')] bg-cover bg-top p-3 rounded-lg">
+    <header className=" w-full bg-[url('/src/assets/images/happy_family.jpg')] bg-[length:200%] bg-bottom bg-no-repeat bg-[rgb(204,195,183)] grid grid-rows-2 p-3 rounded-lg">
       <article className="">
-        <div className="bg-linear-to-t to-blue-200 from-purple-300 px-4 py-6 rounded-t-lg">
-          <h2 className=" text-3xl font-bold">
+        <div className="bg-linear-to-t to-blue-100 from-purple-300 px-4 py-6 rounded-t-lg">
+          <h2 className=" text-2xl font-bold">
             Estás a punto de entrar al universo de la <span className="bg-blue-900 text-white px-1">educación digital</span>
           </h2>
         </div>
-        <div className="flex justify-center p-4 py-8 rounded-b-lg bg-white">
+        <div className="flex justify-center p-4 pt-10 pb-6 rounded-b-lg bg-white">
           <LogoGE />
         </div>
       </article>
